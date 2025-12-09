ff63ddb4b8174e283d56e8428647486775f5e5c8
mFerHer <fhdez.maria@gmail.com>
2025-12-09 19:10:58 +0100

    update db file created

--- update_database.py
+++ update_database.py
@@ -1,14 +1,11 @@
 from datetime import datetime
-import requests     # Import the requests library to handle HTTP requests
-import sqlite3    # Import the sqlite3 library to interact with SQLite databases
+import requests
+import sqlite3
 
 
-# Connect to the SQLite database
 db = sqlite3.connect('data.db')
-# Create a cursor object to execute SQL commands
-curs = db.cursor()
+curs = db.cursor()                 
 
-# CREATE TABLES in the db if it doesn't exist
 curs.execute("""CREATE TABLE IF NOT EXISTS info (
              id INTEGER PRIMARY KEY NOT NULL,
              name TEXT NOT NULL,
@@ -31,22 +28,18 @@ curs.execute("""CREATE TABLE IF NOT EXISTS prices (
 """)
 
 
-# Define the URL for the API endpoint
 url = "https://energia.serviciosmin.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/"   
 
-response = requests.get(url)           # Send a GET request to the specified URL
-data = response.json()                 # Parse the JSON response into a Python dictionary
-stations = data['ListaEESSPrecio']     # Extract the list of fuel stations from the response
+response = requests.get(url)
+data = response.json()
+stations = data['ListaEESSPrecio']
 
-# Convert price strings to float, replacing commas with decimal points
 def convert_price(price):
     if price is None or price == "":
         return None
     return float(price.replace(",", "."))
 
-# GET DATA: Iterate over each station in the list
 for station in stations:
-    # Only consider public stations
     if station['Tipo Venta'] != 'P':
         continue
 
@@ -57,7 +50,6 @@ for station in stations:
     latitude = float(station['Latitud'].replace(",", "."))
     longitude = float(station['Longitud (WGS84)'].replace(",", "."))
 
-    # Insert station info into the database
     curs.execute("""INSERT INTO info (id, name, address, timetable, latitude, longitude)
     VALUES (?, ?, ?, ?, ?, ?)
     ON CONFLICT(id) DO UPDATE SET
@@ -68,14 +60,12 @@ for station in stations:
         longitude = excluded.longitude
     """, (id, name, address, timetable, latitude, longitude))
     
-
     gasoline95 = convert_price(station['Precio Gasolina 95 E5'])
     gasoline98 = convert_price(station['Precio Gasolina 98 E5'])
     diesel = convert_price(station['Precio Gasoleo A'])
     diesel_premium = convert_price(station['Precio Gasoleo Premium'])
     dieselB = convert_price(station['Precio Gasoleo B'])
 
-    # Insert price info into the database
     curs.execute("""INSERT INTO prices (id, gasoline95, gasoline98, diesel, diesel_premium, dieselB)
     VALUES (?, ?, ?, ?, ?, ?)
     ON CONFLICT(id) DO UPDATE SET
@@ -86,7 +76,6 @@ for station in stations:
         dieselB = excluded.dieselB
     """, (id, gasoline95, gasoline98, diesel, diesel_premium, dieselB))
     
-# Save changes & close the database
 db.commit()
 db.close()
 
