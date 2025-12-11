1e6c43d3ef202598f9b94ce6d117c5a5b52c74b6
mFerHer <fhdez.maria@gmail.com>
2025-12-11 20:04:01 +0100

    save

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, render_template
 import sqlite3
 
 
-app = Flask(__name__)
-
 @app.route("/")
+
 def index():
-    
     db = sqlite3.connect("data.db")
     db.row_factory = sqlite3.Row
     curs = db.cursor()
-
-    curs.execute("""SELECT * FROM info
-                    JOIN prices ON info.id = prices.id""")
-
-    stations = curs.fetchall()
+    curs.execute("SELECT * FROM info JOIN prices ON info.id = prices.id")
+    rows = curs.fetchall()
     db.close()
+
+    # Convert sqlite Row objects to dicts so Jinja |tojson works
+    stations = [dict(row) for row in rows]
 
     return render_template("index.html", stations=stations)
